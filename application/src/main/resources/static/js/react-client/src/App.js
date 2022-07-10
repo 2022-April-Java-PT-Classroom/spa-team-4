4ac258d9a1c1aebbb196975d055ba81338064293
jjmirwaldjr <jjmirwaldjr@gmail.com>
2022-07-10 15:24:49 -0400

    Alot of changes, moved folder's and set up app.js

--- application/src/main/resources/static/js/react-client/src/App.js
+++ application/src/main/resources/static/js/react-client/src/App.js
@@ -1,9 +1,14 @@
+import Footer from './Components/footer';
+import Header from './Components/Header';
 import React from 'react';
+import style from "./style.module.scss"
 
 const App = () => {
   return (
     <div>
+      <Header />
       Hello React World!
+      <Footer />
     </div>
   );
 }
--- application/src/main/resources/static/js/react-client/src/Components/footer/index.js
+++ application/src/main/resources/static/js/react-client/src/Components/footer/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./Style.module.scss"
+import style from "./style.module.scss"
 
 const Footer = () =>{
     return(
