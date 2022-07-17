8c50f1cf07801cb1d575e131895995f40730799a
Zaylet Feliciano <zayletfeliciano@Zaylets-MacBook-Pro.local>
2022-07-17 02:32:57 -0400

    Was able to build the museum art api, still having trouble with fecthing the object IDs. Also changed about us page to have the names by alphabetic order and added the names

--- application/src/main/resources/static/js/react-client/src/Pages/Museum-api/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/Museum-api/index.js
@@ -1,1 +1,13 @@
 import React from 'react';
+import Museum from './Museum-Art';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const rootElement = document.getElementById("root");
+const root = createRoot (rootElement);
+
+root.render(
+    <StrictMode>
+        <Museum/>
+    </StrictMode>
+);
