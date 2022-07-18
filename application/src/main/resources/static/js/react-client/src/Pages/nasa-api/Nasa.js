e40d4d0a55087df9023b3ca7b1290834cbbdcefb
jjmirwaldjr <jjmirwaldjr@gmail.com>
2022-07-18 06:35:43 -0400

    got the search bar functioning fully.

--- application/src/main/resources/static/js/react-client/src/Pages/nasa-api/Nasa.js
+++ application/src/main/resources/static/js/react-client/src/Pages/nasa-api/Nasa.js
@@ -1,50 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
+import Axios from "axios"
 import NasaImages from "./NasaImages.js"
-import axios from "axios";
 import style from "./style.module.scss"
 
-// import Axios from "axios"
-
-
-
-
 const Nasa = () => {
-  function nasaApiSearch(){
-    var nasaUrl = 'https://images-api.nasa.gov/search?q=' + document.getElementById('nasaSearch').value;
-    console.log(nasaUrl)
-    return nasaUrl
-  }
-  
+
   const [nasaImages, setImages] = useState(null);
   const [loading, setLoading] = useState(true);
-  
-  useEffect(()=> {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
     const fetchData = async () => {
-      
-      const nasa = await axios.get(nasaApiSearch());
+      const nasa = await Axios(`https://images-api.nasa.gov/search?q=${searchTerm}`);
       setImages(nasa.data.collection.items);
     };
-    
-    
-    if (nasaImages) {
-      setLoading(false);
-    }
-    
-    const timer = setTimeout(() => {
-      !nasaImages && fetchData();
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, [nasaImages]);
-  
+
+    fetchData();
+
+  }
+
+  const searchTextChangeHandler = (e) => {
+    setSearchTerm(e.target.value);
+  }
 
   return (
     <div>
       <h2 className={style.nasaFont}>Nasa Images</h2>
-      <input type="text" id="nasaSearch" placeholder="Search Nasa Database..."/>
-        <button value="Submit" onClick={nasaApiSearch}>Browse the Stars</button>
+      <form onSubmit={submitHandler}>
+        <input type="text" onChange={searchTextChangeHandler} placeholder="Search Nasa Database..." />
+        <button type="submit">Submit</button>
+      </form>
       <section>
-        {loading ? <h3>Loading ...</h3> : <NasaImages images={nasaImages}/>}
+        <NasaImages images={nasaImages} />
       </section>
     </div>
   );
--- application/src/main/resources/static/js/react-client/src/Pages/nasa-api/NasaImages.js
+++ application/src/main/resources/static/js/react-client/src/Pages/nasa-api/NasaImages.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-const NasaImages = ({ images }) => {
-  // console.log(images);
-  return images.map((image, index) => {
+const NasaImages = ({ images}) => {
+ 
+  return images && images.map((image, index) => {
 
-    // console.log(image.links && image.links[0].href);
+    
     return (
       <div key={index}>
-        {/* <p>Test</p> */}
         <img src={image.links && image.links[0].href} alt="" />
         <p></p>
       </div>
