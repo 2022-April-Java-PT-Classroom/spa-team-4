ee47403c9ea14f161fb688cbe78013ded1fc5779
acbluem <acbluem@gmail.com>
2022-07-18 14:34:11 -0400

    added background, changed fonts, added instructions

--- application/src/main/resources/static/js/react-client/src/Pages/User-Gallery/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/User-Gallery/index.js
@@ -1,58 +1,58 @@
-import React,{useEffect, useState} from "react";
+// import React,{useEffect, useState} from "react";
 
 
-const UserGallery = () =>{
-    return(
-        <div className={style.UserGallery}>
-            <h2>Welcome To The User Submitted Gallery</h2>
+// const UserGallery = () =>{
+//     return(
+//         <div className={style.UserGallery}>
+//             <h2>Welcome To The User Submitted Gallery</h2>
 
-import Axios from "axios";
-import style from './style.module.scss';
+// import Axios from "axios";
+// import style from './style.module.scss';
 
-const UserArt = ({userArt}) => {
+// const UserArt = ({userArt}) => {
 
-        const [loadingUserArt, setLoadingUserArt] = useState(true),
-        [gallery, setGallery] = useState(null);
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await Axios('http://localhost:8080/gallery');
-            setGallery(result.data);
-        }
+//         const [loadingUserArt, setLoadingUserArt] = useState(true),
+//         [gallery, setGallery] = useState(null);
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             const result = await Axios('http://localhost:8080/gallery');
+//             setGallery(result.data);
+//         }
 
-        if (gallery) {
-            setLoadingUserArt(false);
-        }
+//         if (gallery) {
+//             setLoadingUserArt(false);
+//         }
 
-        const timer = setTimeout(() => {
-            !gallery && fetchData(); 
-        }, 1000);
-        return () => clearTimeout(timer);
+//         const timer = setTimeout(() => {
+//             !gallery && fetchData(); 
+//         }, 1000);
+//         return () => clearTimeout(timer);
 
-    }, [gallery]);
+//     }, [gallery]);
  
 
-    return (
-        <div>
-            <div className={style.form__container}>
+//     return (
+//         <div>
+//             <div className={style.form__container}>
 
-            </div>
-            {loadingUserArt ? <h3>Loading Gallery ...</h3> :
-                <>
-                    <h2>Here Is Our Gallery Selection</h2>
-                    <ul>
-                        {gallery.map(userArt => (
-                            <div key={userArt.id}>
-                                <p>Artist Name: {userArt.artistName}</p>
-                                <p>Art Description: {userArt.artDesc}</p>
-                                <p>Art Url: {userArt.artUrl}</p>
-                            </div>
-                        ))}
-                    </ul>
-                </>
-            }
+//             </div>
+//             {loadingUserArt ? <h3>Loading Gallery ...</h3> :
+//                 <>
+//                     <h2>Here Is Our Gallery Selection</h2>
+//                     <ul>
+//                         {gallery.map(userArt => (
+//                             <div key={userArt.id}>
+//                                 <p>Artist Name: {userArt.artistName}</p>
+//                                 <p>Art Description: {userArt.artDesc}</p>
+//                                 <p>Art Url: {userArt.artUrl}</p>
+//                             </div>
+//                         ))}
+//                     </ul>
+//                 </>
+//             }
 
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
 
-export default UserArt;
+// export default UserArt;
