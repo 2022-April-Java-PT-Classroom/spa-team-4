22e3a0acd52b95464c53a615dfa1b99ddb7f29f6
Zaylet Feliciano <zayletfeliciano@Zaylets-MacBook-Pro.local>
2022-07-20 09:45:46 -0400

    now is supposed to show

--- application/src/main/resources/static/js/react-client/src/Pages/Museum-api/Museum-Art.js
+++ application/src/main/resources/static/js/react-client/src/Pages/Museum-api/Museum-Art.js
@@ -5,13 +5,12 @@ import style from './style.module.scss';
 const Museum = () => {
     const [artWork, setArtWork] = useState(null);
     const [artWorkNumTwo, setArtWorkNumTwo] = useState(null);
-    const [artWorkNumThree, setArtWorkNumThree] = useState(null);
     const [loading, setLoading] = useState(true);
     
     useEffect(() => {
 
         const fetchData = async () => {
-            const museumArt = await Axios("https://collectionapi.metmuseum.org/public/collection/v1/objects/500");
+            const museumArt = await Axios("https://collectionapi.metmuseum.org/public/collection/v1/objects/437");
 
             setArtWork(museumArt.data);
             
@@ -28,70 +27,46 @@ const Museum = () => {
     }, [artWork]);
 
     useEffect(() => {
-        const fetchData = async () => {
-            const museumArt = await Axios("https://collectionapi.metmuseum.org/public/collection/v1/objects/527");
 
+        const fetchData= async () => {
+            const museumArt = await Axios("https://collectionapi.metmuseum.org/public/collection/v1/objects/500");
             setArtWorkNumTwo(museumArt.data);
-        };
-
+        }
         if (artWorkNumTwo) {
             setLoading(false);
         }
 
-        const timer = setTimeout(() =>{
+        const timer = setTimeout(() => {
             !artWorkNumTwo && fetchData();
         }, 1000);
         return () => clearTimeout(timer);
     }, [artWorkNumTwo]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const museumArt = await Axios("https://collectionapi.metmuseum.org/public/collection/v1/objects/40");
-
-            setArtWorkNumThree(museumArt.data);
-        };
-
-        if (artWorkNumThree) {
-            setLoading(false);
-        }
-
-        const timer = setTimeout(() =>{
-            !artWorkNumThree && fetchData();
-        }, 1000);
-        return () => clearTimeout(timer);
-    }, [artWorkNumThree]);
-    
 
     return (
         loading ? <h3>Loading Art...</h3> :
         <div className={style.singlePage}>
             <h2 className={style.metroHeader}>The Metropolitan Museum of Art</h2>
             <section className={style.art}>
-                <p>{artWork.department}</p>
                 <p>{artWork.title}</p>
                 <p>{artWork.artistDisplayName}</p>
+                <p>{artWork.department}</p>
                 <p>{artWork.objectDate}</p>
                 <p>{artWork.repository}</p>
                 <div className={style.artImg}>
-                <img src={artWork.primaryImage} height="500" width="500"></img>
+                    <img src={artWork.primaryImage} height="500" width="500"></img>
                 </div>
-                <p>{artWorkNumTwo.department}</p>
+            </section> 
+            <section className={style.art}>
                 <p>{artWorkNumTwo.title}</p>
                 <p>{artWorkNumTwo.artistDisplayName}</p>
+                <p>{artWorkNumTwo.department}</p>
                 <p>{artWorkNumTwo.objectDate}</p>
                 <p>{artWorkNumTwo.repository}</p>
                 <div className={style.artImg}>
-                <img src={artWorkNumTwo.primaryImage} height="500" width="500"></img>
-                </div>
-                <p>{artWorkNumThree.department}</p>
-                <p>{artWorkNumThree.title}</p>
-                <p>{artWorkNumThree.artistDisplayName}</p>
-                <p>{artWorkNumThree.objectDate}</p>
-                <p>{artWorkNumThree.repository}</p>
-                <div className={style.artImg}>
-                <img src={artWorkNumThree.primaryImage} height="500" width="500"></img>
+                    <img src={artWorkNumTwo.primaryImage} height="500" width="500"></img>
                 </div>
             </section>
+
         </div>
     );
 }
