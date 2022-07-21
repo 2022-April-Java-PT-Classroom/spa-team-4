2ec4b476fe7b45043aa303c2edf703b5cb9eb697
anderafells <fellsandera@gmail.com>
2022-07-21 18:42:27 -0400

    added start of styling

--- application/src/main/resources/static/js/react-client/src/Pages/about-us/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/about-us/index.js
@@ -3,79 +3,82 @@ import style from './style.module.scss';
 
 const AboutUs = () => {
     return (
-        <div className ={style.aboutUs}>
-            <h2>About Us</h2>
-            <h3>Aisha Bluem</h3>
-            <img src="images/Aisha.jpg" height="200" width="200"></img>
-            <a href="https://github.com/acbluem">GitHub</a>
-            <a href="https://www.linkedin.com/in/aishabluem/">LinkedIn</a>
-            <p>Hi! My name is Aisha (phonetic spelling: ay-zhuh).
-               I am a junior full-stack software developer pursuing my passion in coding, reigniting the days of my youth
-                where I'd make front-end websites of my favorite cartoon/video game characters in the early and late 2000s.
-                 I am a highly focused problem solver with excellent research and troubleshooting skills, committed to
-                 breaking into the tech industry.</p>
 
-            <h3> Andera Fells</h3>
-            <img src="images/Sade.jpg" height="200" width="200"></img>
-            <a href="https://github.com/anderafells">GitHub</a>
-            <a href="https://www.linkedin.com/in/anderafells/">LinkedIn</a>
-            <p> I am a full stack developer currently located in Columbus, OH. My love for learning,and taking on challe
-            -nging task has set me on the path of coding. I love to see projects come "alive". I am a dedicated team
-            player, but I also work well on my own. Outside of development I enjoy researching anything that catches
-            my attention from music,dance, how the mind works etc.. Most importantly I love spending time with my family!</p>
-
-            <h3>Dane Firestone</h3>
-           <img src="images/dane.png" height="200" width="200"></img>
-            <a href="https://github.com/danefirestone">GitHub</a>
-            <a href="https://www.linkedin.com/in/danefirestone/">LinkedIn</a>
-            <p>My passion for tech started as kid and hasn't stopped evolving. After starting my own Web Design & IT
-            business that focused on developing small businesses in my rural hometown. I took an opportunity to move to
-            the Harrisburg area and start at Amazon. Amazon has provided me the space to grow my career in Information
-            Technology and continually expand my knowledge. I am currently pursuing Full Stack Development through WCCI
-            and cannot wait to see what awaits.</p>
-
-            <h3>Jason Mirwald</h3>
-            <img src="images/Jason.png" height="200" width="200"></img>
-            <a href="https://github.com/jjmirwaldjr">GitHub</a>
-            <a href="https://www.linkedin.com/in/jason-mirwald-jr-8694a1236/">LinkedIn</a>
-            <p>I am a Military Veteran, Served in the US navy as a Logistic Specialist. I was Honorably
-               Discharged. I spent the next few years working in the customer service industry and
-               developing management skills through working different part-time jobs. Now I work at
-               Amazon. I also have technical experience, between Graphics design and media editing. I am
-               now working on developing my coding skills through the We Can Code It boot camp. I have
-               done a few years at the art institute to learn many different skills, Graphics design, Media
-               Editing, 3D modeling and animations, 2D animation and Photoshop skills. Now I'm looking to
-               continue to expand my skills by picking up both back end and front end development. </p>
-
-            <h3>Louis Tchamba</h3>
-            <img src="images/Louis.jpg" height="200" width="200"></img>
-            <a href="https://github.com/louis2tch">GitHub</a>
-            <a href="https://www.linkedin.com/in/louis-tchamda-9589bb74/">LinkedIn</a>
-            <p>Full stack developer.
-               Currently taking “WCCI Bootcamp” - completion date: August 2022.
-               Previously, I usualy worked on Client/Server Databases: coding, providing php-mysql soulutions and support
-                for various Information Systems. For 15 years I have designed, developed and maintained different systems,
-                specifically commercial websites, CRM(customer relationship management) and software. I have advanced
-                experience in code analysis, written by other developers, particularly in PHP.</p>
-
-            <h3>Victor Negron</h3>
-            <img src="images/Victor.jpg" height="200" width="200"></img>
-            <a href="https://github.com/VictorLNegron">GitHub</a>
-            <a href="https://www.linkedin.com/in/victor-negron-742558235/">LinkedIn</a>
-            <p>Hey im Victor, Born in Cleveland, Ohio, I've always wanted to take the leap towards a career in technol
-            -ogy but kept my plans on hold as life got in the way. But in the mean time, I decided to learn to code on
-            my own as I always felt drawn to it. After I get my Certificate in Software Development, I will finally be
-            able to put it to use and realize my dream of transitioning into a career in tech!</p>
-
-            <h3>Zaylet Feliciano</h3>
-            <img src="images/Zaylet.png" height="200" width="200"></img>
-            <a href="https://github.com/zaylet">GitHub</a>
-            <a href="https://www.linkedin.com/in/zayletfeliciano/">LinkedIn</a>
-            <p>My name is Zaylet Feliciano, I am a Software Developer who loves to code and learn something new everyday.
-             In 2022, I decided to study at We Can Code IT to be able to pursue this career in the hopes to better my
-                future and start my passion for coding. I hope you enjoy our website as much as I do!</p>
-        </div>
-    );
-}
+                <div className ={style.aboutUs}>
+                    <h2>About Us</h2>
+                    <h3>Aisha Bluem</h3>
+                    <img src="images/Aisha.jpg" height="200" width="200"></img>
+                    <a href="https://github.com/acbluem">GitHub</a>
+                    <a href="https://www.linkedin.com/in/aishabluem/">LinkedIn</a>
+                    <p> Hi! My name is Aisha (phonetic spelling: ay-zhuh).
+                       I am a junior full-stack software developer pursuing my passion in coding, reigniting the days of my youth
+                        where I'd make front-end websites of my favorite cartoon/video game characters in the early and late 2000s.
+                         I am a highly focused problem solver with excellent research and troubleshooting skills, committed to
+                         breaking into the tech industry.</p>
+           
+                    <h3> Andera Fells</h3>
+                    <img src="images/Sade.jpg" height="200" width="200"></img>
+                    <a href="https://github.com/anderafells">GitHub</a>
+                    <a href="https://www.linkedin.com/in/anderafells/">LinkedIn</a>
+                    <p> I am a full stack developer currently located in Columbus, OH. My love for learning,and taking on challe
+                    -nging task has set me on the path of coding. I love to see projects come "alive". I am a dedicated team
+                    player, but I also work well on my own. Outside of development I enjoy researching anything that catches
+                    my attention from music,dance, how the mind works etc.. Most importantly I love spending time with my family!</p>
+        
+                    <h3>Dane Firestone</h3>
+                   <img src="images/dane.png" height="200" width="200"></img>
+                    <a href="https://github.com/danefirestone">GitHub</a>
+                    <a href="https://www.linkedin.com/in/danefirestone/">LinkedIn</a>
+                    <p>My passion for tech started as kid and hasn't stopped evolving. After starting my own Web Design & IT
+                    business that focused on developing small businesses in my rural hometown. I took an opportunity to move to
+                    the Harrisburg area and start at Amazon. Amazon has provided me the space to grow my career in Information
+                    Technology and continually expand my knowledge. I am currently pursuing Full Stack Development through WCCI
+                    and cannot wait to see what awaits.</p>
+        
+                    <h3>Jason Mirwald</h3>
+                    <img src="images/Jason.png" height="200" width="200"></img>
+                    <a href="https://github.com/jjmirwaldjr">GitHub</a>
+                    <a href="https://www.linkedin.com/in/jason-mirwald-jr-8694a1236/">LinkedIn</a>
+                    <p>I am a Military Veteran, Served in the US navy as a Logistic Specialist. I was Honorably
+                       Discharged. I spent the next few years working in the customer service industry and
+                       developing management skills through working different part-time jobs. Now I work at
+                       Amazon. I also have technical experience, between Graphics design and media editing. I am
+                       now working on developing my coding skills through the We Can Code It boot camp. I have
+                       done a few years at the art institute to learn many different skills, Graphics design, Media
+                       Editing, 3D modeling and animations, 2D animation and Photoshop skills. Now I'm looking to
+                       continue to expand my skills by picking up both back end and front end development. </p>
+        
+                    <h3>Louis Tchamba</h3>
+                    <img src="images/Louis.jpg" height="200" width="200"></img>
+                    <a href="https://github.com/louis2tch">GitHub</a>
+                    <a href="https://www.linkedin.com/in/louis-tchamda-9589bb74/">LinkedIn</a>
+                    <p>Full stack developer.
+                       Currently taking “WCCI Bootcamp” - completion date: August 2022.
+                       Previously, I usualy worked on Client/Server Databases: coding, providing php-mysql soulutions and support
+                        for various Information Systems. For 15 years I have designed, developed and maintained different systems,
+                        specifically commercial websites, CRM(customer relationship management) and software. I have advanced
+                        experience in code analysis, written by other developers, particularly in PHP.</p>
+        
+                    <h3>Victor Negron</h3>
+                    <img src="images/Victor.jpg" height="200" width="200"></img>
+                    <a href="https://github.com/VictorLNegron">GitHub</a>
+                    <a href="https://www.linkedin.com/in/victor-negron-742558235/">LinkedIn</a>
+                    <p>Hey im Victor, Born in Cleveland, Ohio, I've always wanted to take the leap towards a career in technol
+                    -ogy but kept my plans on hold as life got in the way. But in the mean time, I decided to learn to code on
+                    my own as I always felt drawn to it. After I get my Certificate in Software Development, I will finally be
+                    able to put it to use and realize my dream of transitioning into a career in tech!</p>
+        
+                    <h3>Zaylet Feliciano</h3>
+                    <img src="images/Zaylet.png" height="200" width="200"></img>
+                    <a href="https://github.com/zaylet">GitHub</a>
+                    <a href="https://www.linkedin.com/in/zayletfeliciano/">LinkedIn</a>
+                    <p>My name is Zaylet Feliciano, I am a Software Developer who loves to code and learn something new everyday.
+                     In 2022, I decided to study at We Can Code IT to be able to pursue this career in the hopes to better my
+                        future and start my passion for coding. I hope you enjoy our website as much as I do!</p>
+                </div>
+                
+            );
+        }
+        
 
 export default AboutUs;
