4b6a3bab37f6b23154791ba5ebc1fd60fe80eb2b
louis2tch <louis2tch@gmail.com>
2022-07-20 20:22:06 -0400

    all

--- application/src/main/java/org/wecancodeit/serverside/Populator.java
+++ application/src/main/java/org/wecancodeit/serverside/Populator.java
@@ -15,7 +15,7 @@ public class Populator implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        UserArt jasonArt = new UserArt("Jason Mirwald", "title1", "artDesc", "artUrl");
+        UserArt jasonArt = new UserArt("Jason Mirwald", "title1", "artDesc", "https://th.bing.com/th/id/R.e6ed9917b25ea49e39559fa40889c6dd?rik=jhQU9jcnR1uktA&pid=ImgRaw&r=0");
         userArtRepo.save(jasonArt);
         UserArt test = new UserArt("john ", "title1", "Landscape", "https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
         userArtRepo.save(test);
--- application/src/main/java/org/wecancodeit/serverside/controllers/UserArtController.java
+++ application/src/main/java/org/wecancodeit/serverside/controllers/UserArtController.java
@@ -22,13 +22,20 @@ public class UserArtController {
         return (Collection<UserArt>) userArtRepo.findAll();
     }
 
-    @PostMapping("/gallery/add-art")
+    @PostMapping("/api/gallery/add-art")
     public Collection<UserArt> addUserArt(@RequestBody String body) throws JSONException {
         JSONObject newUserArt = new JSONObject(body);
         String artistName = newUserArt.getString("artistName");
         String artTitle = newUserArt.getString("artTitle");
         String artDesc = newUserArt.getString("artDesc");
         String artUrl = newUserArt.getString("artUrl");
+
+        //Optional<UserArt> userArtToAddOpt = userArtRepo.findByName(artTitle);
+        //add UserArt if his title not already exist in the database (so to avoid repeating titles)
+       // if (userArtToAddOpt.isEmpty()) {
+            UserArt userArtToAdd = new UserArt(artistName, artTitle, artDesc, artUrl);
+            userArtRepo.save(userArtToAdd);
+       // }
 
         return (Collection<UserArt>) userArtRepo.findAll();
     }
