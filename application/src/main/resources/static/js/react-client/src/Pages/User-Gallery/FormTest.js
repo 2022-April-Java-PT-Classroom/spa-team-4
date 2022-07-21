87cb633fc83ab8f013c1ae18eafa7ccbab4665bb
VictorLNegron <negronvictor882@gmail.com>
2022-07-21 18:35:36 -0400

    Got the form to work also refresh the page after you press the add button

---
+++ application/src/main/resources/static/js/react-client/src/Pages/User-Gallery/FormTest.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+import UserArt from '.';
+import axios from 'axios';
+import style from './style.module.scss';
+
+const UserSubmmited = ({userSubmmited}) => {
+
+    
+
+    const [userArtState, setUserArtState] = useState({
+        artistName: "",
+        artTitle: "",
+        artDesc: "",
+        artUrl: ""
+    });
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setUserArtState({
+            ...userArtState,
+            [e.target.name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const artistData = {
+            artistName: userArtState.artistName,
+            artTitle: userArtState.artTitle,
+            artDesc: userArtState.artDesc,
+            artUrl: userArtState.artUrl
+        };
+
+        axios.post('http://localhost:8080/api/gallery/add-art', artistData).then((response) => {
+            console.log(response.status);
+            console.log('DATA', response.data);
+            setUserArtState(response.data);
+        });
+    };
+
+
+    return (
+        <div className={style.form__container}>
+    <form onSubmit={handleSubmit}>
+                    <input type="text" name="artistName" value={UserArt.artistName} onChange={handleChange} placeholder='Enter the artist Name' />
+                    <input type="text" name="artTitle" value={UserArt.artTitle} onChange={handleChange} placeholder='Enter the art Title' />
+                    <textarea name="artDesc" value={UserArt.artDesc} onChange={handleChange} placeholder='Enter the art Description' />
+                    <input type="text" name="artUrl" value={UserArt.artUrl} onChange={handleChange} placeholder='Enter the art Url picture' />
+                    <button type="submit">Add item in Gallery</button>
+                </form>
+           
+           
+        </div>
+
+    );
+}
+
+export default UserSubmmited;
