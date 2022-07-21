8bbca693978ca6d3cca5796398d537234c5016e9
VictorLNegron <negronvictor882@gmail.com>
2022-07-21 17:00:44 -0400

    Removed unnecessary things

--- application/src/main/resources/static/js/react-client/src/Components/ImageDisplay/ImageComponent.js
+++ application/src/main/resources/static/js/react-client/src/Components/ImageDisplay/ImageComponent.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 
 class ImageComponent extends Component {
-    constructor(props) {
-        super(props);
-    }
+    
     render() {
         return (
             <div>
-                <img src={this.props.url} alt="display image" />
-                <img src={this.props.url} alt="display image" />
+                
+                <img src={this.props.url} alt="display Art" />
 
             </div>
         );
