b04bad400ef3078362abe3050ef3197d95777171
anderafells <fellsandera@gmail.com>
2022-07-22 09:00:43 -0400

    finished styling. All done!

--- application/src/main/resources/static/js/react-client/src/Pages/about-us/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/about-us/index.js
@@ -6,8 +6,9 @@ const AboutUs = () => {
 
                 <div className ={style.aboutUs}>
                     <h2>About Us</h2>
-                    <h3>Aisha Bluem</h3>
+                    <div className={style.container}>
                     <img src="images/Aisha.jpg" height="200" width="200"></img>
+                    <h3>Aisha Bluem</h3>
                     <a href="https://github.com/acbluem">GitHub</a>
                     <a href="https://www.linkedin.com/in/aishabluem/">LinkedIn</a>
                     <p> Hi! My name is Aisha (phonetic spelling: ay-zhuh).
@@ -15,9 +16,9 @@ const AboutUs = () => {
                         where I'd make front-end websites of my favorite cartoon/video game characters in the early and late 2000s.
                          I am a highly focused problem solver with excellent research and troubleshooting skills, committed to
                          breaking into the tech industry.</p>
-           
-                    <h3> Andera Fells</h3>
+                    
                     <img src="images/Sade.jpg" height="200" width="200"></img>
+                    <h3> Andera Fells</h3>
                     <a href="https://github.com/anderafells">GitHub</a>
                     <a href="https://www.linkedin.com/in/anderafells/">LinkedIn</a>
                     <p> I am a full stack developer currently located in Columbus, OH. My love for learning,and taking on challe
@@ -25,8 +26,9 @@ const AboutUs = () => {
                     player, but I also work well on my own. Outside of development I enjoy researching anything that catches
                     my attention from music,dance, how the mind works etc.. Most importantly I love spending time with my family!</p>
         
-                    <h3>Dane Firestone</h3>
+                    
                    <img src="images/dane.png" height="200" width="200"></img>
+                   <h3>Dane Firestone</h3>
                     <a href="https://github.com/danefirestone">GitHub</a>
                     <a href="https://www.linkedin.com/in/danefirestone/">LinkedIn</a>
                     <p>My passion for tech started as kid and hasn't stopped evolving. After starting my own Web Design & IT
@@ -35,8 +37,9 @@ const AboutUs = () => {
                     Technology and continually expand my knowledge. I am currently pursuing Full Stack Development through WCCI
                     and cannot wait to see what awaits.</p>
         
-                    <h3>Jason Mirwald</h3>
+                    
                     <img src="images/Jason.png" height="200" width="200"></img>
+                    <h3>Jason Mirwald</h3>
                     <a href="https://github.com/jjmirwaldjr">GitHub</a>
                     <a href="https://www.linkedin.com/in/jason-mirwald-jr-8694a1236/">LinkedIn</a>
                     <p>I am a Military Veteran, Served in the US navy as a Logistic Specialist. I was Honorably
@@ -48,19 +51,21 @@ const AboutUs = () => {
                        Editing, 3D modeling and animations, 2D animation and Photoshop skills. Now I'm looking to
                        continue to expand my skills by picking up both back end and front end development. </p>
         
-                    <h3>Louis Tchamba</h3>
+                    
                     <img src="images/Louis.jpg" height="200" width="200"></img>
-                    <a href="https://github.com/louis2tch">GitHub</a>
-                    <a href="https://www.linkedin.com/in/louis-tchamda-9589bb74/">LinkedIn</a>
+                    <h3>Louis Tchamba</h3>
+                    <a href=  "https://github.com/louis2tch">GitHub</a>
+                    <a href=  "https://www.linkedin.com/in/louis-tchamda-9589bb74/">LinkedIn</a>
                     <p>Full stack developer.
-                       Currently taking “WCCI Bootcamp” - completion date: August 2022.
-                       Previously, I usualy worked on Client/Server Databases: coding, providing php-mysql soulutions and support
-                        for various Information Systems. For 15 years I have designed, developed and maintained different systems,
-                        specifically commercial websites, CRM(customer relationship management) and software. I have advanced
-                        experience in code analysis, written by other developers, particularly in PHP.</p>
+                       Currently taking “WCCI Bootcamp” - completion date: August 2022.Previously, I usualy worked on
+                       Client/Server Databases: coding, providing php-mysql soulutions and support for various
+                       Information Systems. For 15 years I have designed, developed and maintained different systems,
+                       specifically commercial websites, CRM(customer relationship management) and software. I have
+                       advanced experience in code analysis, written by other developers, particularly in PHP.</p>
         
-                    <h3>Victor Negron</h3>
+                   
                     <img src="images/Victor.jpg" height="200" width="200"></img>
+                    <h3>Victor Negron</h3>
                     <a href="https://github.com/VictorLNegron">GitHub</a>
                     <a href="https://www.linkedin.com/in/victor-negron-742558235/">LinkedIn</a>
                     <p>Hey im Victor, Born in Cleveland, Ohio, I've always wanted to take the leap towards a career in technol
@@ -68,14 +73,17 @@ const AboutUs = () => {
                     my own as I always felt drawn to it. After I get my Certificate in Software Development, I will finally be
                     able to put it to use and realize my dream of transitioning into a career in tech!</p>
         
-                    <h3>Zaylet Feliciano</h3>
+                    
                     <img src="images/Zaylet.png" height="200" width="200"></img>
+                    <h3>Zaylet Feliciano</h3>
                     <a href="https://github.com/zaylet">GitHub</a>
                     <a href="https://www.linkedin.com/in/zayletfeliciano/">LinkedIn</a>
                     <p>My name is Zaylet Feliciano, I am a Software Developer who loves to code and learn something new everyday.
                      In 2022, I decided to study at We Can Code IT to be able to pursue this career in the hopes to better my
                         future and start my passion for coding. I hope you enjoy our website as much as I do!</p>
                 </div>
+                </div>
+
                 
             );
         }
