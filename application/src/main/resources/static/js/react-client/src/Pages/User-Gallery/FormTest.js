ac0cf7e44b9f1e23c08bebdfac0b9d95c84fd406
Zaylet Feliciano <zayletfeliciano@Zaylets-MacBook-Pro.local>
2022-07-22 01:23:18 -0400

    changed the background of the museum page, added a background to the art galler page and Aisha helped with centering the description box and the button on the art gallery page

--- application/src/main/resources/static/js/react-client/src/Pages/User-Gallery/FormTest.js
+++ application/src/main/resources/static/js/react-client/src/Pages/User-Gallery/FormTest.js
@@ -48,7 +48,9 @@ const UserSubmmited = ({userSubmmited}) => {
                     <input type="text" name="artTitle" value={UserArt.artTitle} onChange={handleChange} placeholder='Enter the art Title' />
                     <textarea name="artDesc" value={UserArt.artDesc} onChange={handleChange} placeholder='Enter the art Description' />
                     <input type="text" name="artUrl" value={UserArt.artUrl} onChange={handleChange} placeholder='Enter the art Url picture' />
+                    <div className={style.centerButton}>
                     <button type="submit">Add item in Gallery</button>
+                    </div>
                 </form>
            
            
